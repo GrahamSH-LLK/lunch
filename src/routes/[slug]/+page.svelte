fa4dd78c1543ae431c4ad76bc160720f95374098
GrahamSH <grahamshllk@gmail.com>
2022-08-23 15:16:35 +0000

    Pwetty!

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,35 +1,44 @@
 <script>
 	import { goto } from '$app/navigation';
-	import { browser } from "$app/env";
+	import { browser } from '$app/env';
 
 	import '../../app.css';
-	import { DateInput } from 'date-picker-svelte'
-
+	import { DateInput } from 'date-picker-svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 	$: today = data.today;
 	let date = data.date;
 	$: {
-			if(browser) goto(`/${encodeURIComponent(date.toLocaleString(undefined, {day: '2-digit', year: 'numeric', month: '2-digit'}))}`, { replaceState: true })
-		console.log(date)
+		if (browser)
+			goto(
+				`/${encodeURIComponent(
+					date.toLocaleString(undefined, { day: '2-digit', year: 'numeric', month: '2-digit' })
+				)}`,
+				{ replaceState: true }
+			);
+		console.log(date);
 	}
 </script>
 
-<div class="m-4">
+<div
+	class="flex w-full px-6 py-4 bg-blue-400 justify-between content-center  text-white"
+>
+	<span class=" font-bold text-3xl">🍽️ LHS Lunch</span> <DateInput bind:value={date} />
+</div>
+<div class="m-4 ml-8">
 	<ul>
-		<DateInput bind:value={date} />
-
 		{#each today as category}
 			<li>
-				<h2 class="font-semibold text-2xl">{category.name}</h2>
-				<ul class="bg-red">
+				<h2 class="font-semibold text-2xl my-1">{category.name}</h2>
+				<div class="flex overflow-scroll w-full">
 					{#each category.items as meal}
-						<li>
-							{meal.name}
-						</li>
+						<div class="h-48 w-60 bg-white rounded-md shadow-sm mr-2  shrink-0	">
+							<div style={`background-image: url(${meal.image});`} class="h-32 w-full rounded-t-md bg-cover" />
+							<span class="m-2 font-bold ">{meal.name}</span>
+						</div>
 					{/each}
-				</ul>
+					</div>
 			</li>
 		{/each}
 	</ul>
