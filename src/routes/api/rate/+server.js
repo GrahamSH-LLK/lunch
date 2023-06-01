55b21df341226e2d3894d956702b4b228bddea68
GrahamSH <grahamshllk@gmail.com>
2023-06-01 12:24:46 +0000

    meh?

--- src/routes/api/rate/+server.js
+++ src/routes/api/rate/+server.js
@@ -13,8 +13,8 @@ export async function POST({request}) {
 				'accept-language': 'en-US,en;q=0.9',
 				'content-type': 'application/json;charset=UTF-8'
 			},
-			referrer: 'https://www.schoolnutritionandfitness.com/webmenus2/',
-			referrerPolicy: 'strict-origin-when-cross-origin',
+			//referrer: 'https://www.schoolnutritionandfitness.com/webmenus2/',
+			//referrerPolicy: 'strict-origin-when-cross-origin',
 			body: `{"sid":"1624626026767","product_id":"${product_id}","rate":${rating},"profile_uuid":"${uuid}"}`,
 			method: 'POST',
 			//credentials: 'include'
