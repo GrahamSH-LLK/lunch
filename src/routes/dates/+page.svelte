6d4d8024dc64125c6482b805bc06d2bb744b58d1
GrahamSH <grahamshllk@gmail.com>
2024-07-19 12:08:15 +0000

    make it carded

--- src/routes/dates/+page.svelte
+++ src/routes/dates/+page.svelte
@@ -2,31 +2,36 @@
 	export let data;
 
 	import { Doughnut } from 'svelte-chartjs';
+
 	import Nav from '$lib/Nav.svelte';
+	import Card from '$lib/Card.svelte';
 	import 'chart.js/auto';
 </script>
 
 <Nav emoji="📅" pagename="Dates"></Nav>
 
-<div class="flex flex-row">
-	<div >
-		<h3 class="text-3xl">Normalized Fridays</h3>
-		<span class="italic font-light text-gray-500">Alike days are grouped</span>
-		<div class="h-80 w-80">
-			<Doughnut
-				data={{
-					labels: Object.keys(data.fridays.countPerDayReduced).map(
-						(x) => `Day ${x} or ${parseInt(x) + 4}`
-					),
-					datasets: [
-						{
-							data: Object.values(data.fridays.countPerDayReduced)
-						}
-					]
-				}}
-				options={{ responsive: true }}
-			></Doughnut>
-		</div>
+<div class="flex md:flex-row  flex-col m-6 gap-2">
+	<div>
+		<Card>
+			<h3 class="text-3xl">Normalized Fridays</h3>
+			<span class="italic font-light text-gray-500">Alike days are grouped</span>
+			<div class="h-80 w-80">
+				<Doughnut
+					data={{
+						labels: Object.keys(data.fridays.countPerDayReduced).map(
+							(x) => `Day ${x} or ${parseInt(x) + 4}`
+						),
+						datasets: [
+							{
+								data: Object.values(data.fridays.countPerDayReduced)
+							}
+						]
+					}}
+					options={{ responsive: true }}
+				></Doughnut>
+			</div>
+		</Card>
+		<Card>
 		<h3 class="text-3xl">All Fridays</h3>
 		<div class="h-80 w-80">
 			<Doughnut
@@ -40,9 +45,10 @@
 				}}
 				options={{ responsive: true }}
 			></Doughnut>
-		</div>
+		</div></Card>
 	</div>
 	<div>
+		<Card>
 		<h3 class="text-3xl">Normalized Days</h3>
 		<span class="italic font-light text-gray-500">Alike days are grouped</span>
 
@@ -61,6 +67,8 @@
 				options={{ responsive: true }}
 			></Doughnut>
 		</div>
+	</Card>
+	<Card>
 
 		<h3 class="text-3xl">All Days</h3>
 		<div class="h-80 w-80">
@@ -76,5 +84,6 @@
 				options={{ responsive: true }}
 			></Doughnut>
 		</div>
+	</Card>
 	</div>
 </div>
