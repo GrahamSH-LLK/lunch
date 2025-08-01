5b4e6045217bae1c4d22c94995f91a567b4f763e
Graham <grahamshllk@gmail.com>
2025-07-31 20:37:48 -0400

    Update +page.js

--- src/routes/dates/+page.js
+++ src/routes/dates/+page.js
@@ -1,6 +1,6 @@
 export const load = async () => {
 	const res = await fetch(
-		`https://clients6.google.com/calendar/v3/calendars/longmeadow.k12.ma.us_giqhrfti31osu08rr36po3pvl0@group.calendar.google.com/events?calendarId=longmeadow.k12.ma.us_giqhrfti31osu08rr36po3pvl0%40group.calendar.google.com&singleEvents=true&eventTypes=default&eventTypes=focusTime&eventTypes=outOfOffice&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2024-08-15T00%3A00%3A00-04%3A00&timeMax=2025-07-15T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs&%24unique=gc237`
+		`https://clients6.google.com/calendar/v3/calendars/longmeadow.k12.ma.us_giqhrfti31osu08rr36po3pvl0@group.calendar.google.com/events?calendarId=longmeadow.k12.ma.us_giqhrfti31osu08rr36po3pvl0%40group.calendar.google.com&singleEvents=true&eventTypes=default&eventTypes=focusTime&eventTypes=outOfOffice&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2025-08-15T00%3A00%3A00-04%3A00&timeMax=2026-07-15T00%3A00%3A00-04%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs&%24unique=gc237`
 	);
 	const data = await res.json();
 
